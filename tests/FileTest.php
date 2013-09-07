b1ad11e475a88d0e44125ac3e152aa8bfc123f62
César D. Rodas <crodas@php.net>
2013-09-06 22:56:17 -0400

    Added helper function to write files atomically in PHPD

---
+++ tests/FileTest.php
@@ -0,0 +1,15 @@
+<?php
+
+use crodas\File;
+
+class FileTest extends \phpunit_framework_testcase
+{
+    public function testWrite()
+    {
+        $this->assertFalse(is_file('foobar.php'));
+        File::write("foobar.php", "foobar");
+        $this->assertTrue(is_file('foobar.php'));
+        unlink('foobar.php');
+    }
+}
+
