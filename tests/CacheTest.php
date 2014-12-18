7ace261903a3f5ec9edd0328bfb358b26ef5ddd9
Cesar Rodas <crodas@php.net>
2014-12-18 20:28:12 -0300

    Fixed minor issue in the test suite

--- tests/CacheTest.php
+++ tests/CacheTest.php
@@ -21,7 +21,10 @@ class CacheTest extends \phpunit_framework_testcase
     public function testFirst()
     {
         global $foo, $counter;
-        unlink(File::generateFilepath('class_cache', 'fooClass'));
+        $file = File::generateFilepath('class_cache', 'fooClass');
+        if (is_file($file)) {
+            unlink($file);
+        }
         $proxy = new Cache('fooClass');
         $this->assertEquals($proxy->foobar(1, 2, 3), $proxy->foobar(1, 2, 3));
         $this->assertEquals($proxy->foobar(1, 2, 3), $proxy->foobar(1, 2, 3));
